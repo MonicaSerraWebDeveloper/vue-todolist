cebed4834521dbe4840f744e4b989e844d02a230
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-04-02 18:42:08 +0200

    feat: add the v-for to show all the data in the html as a list and if the key done is true we add the class "marked" with a line-through

--- js/script.js
+++ js/script.js
@@ -3,20 +3,23 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      todo: [
-        {
-            text: 'Fare i compiti',
-            done: true
-        },
-        {
-            text: 'Fare la spesa',
-            done: false
-        },
-        {
-            text: 'Fare il bucato',
-            done: true
-        },
-      ]
+        todo: [
+            {
+                text: 'Fare i compiti',
+                done: true
+            },
+            {
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                text: 'Fare il bucato',
+                done: true
+            },
+        ]
     }
-  }
+  },
+  methods: {
+  
+  },
 }).mount('#app')
